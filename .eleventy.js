4f855f2844b5adf691b93597f2590b0d634046eb
chrisgebert <cj@chrisgebert.org>
2024-08-10 10:59:46 -0400

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 module.exports = function(eleventyConfig) {
     // Set custom directories for input, output, includes, and data
-    eleventyConfig.addPassthroughCopy("assets/css");
-    eleventyConfig.addPassthroughCopy("assets/images");
-    eleventyConfig.addPassthroughCopy("assets/js");
+    eleventyConfig
+        .addPassthroughCopy("src/assets/css")
+        .addPassthroughCopy("src/assets/images")
+        .addPassthroughCopy("src/assets/js");
 
     return {
         passthroughFileCopy: true,
