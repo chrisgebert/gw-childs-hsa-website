b1e7ab8c51f391c082ebce558301d6297c9f446f
chrisgebert <cj@chrisgebert.org>
2024-09-06 21:57:52 -0400

    add event data

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,32 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     // Set custom directories for input, output, includes, and data
+    eleventyConfig.addFilter("futureEvents", function(events) {
+        const now = DateTime.now();
+        const sixWeeksLater = now.plus({ weeks: 6});
+
+        return events.filter(event => {
+            const eventDate = DateTime.fromISO(event.date);
+            return eventDate >= now && eventDate <= sixWeeksLater;
+        });
+    });
+
+    eleventyConfig.addFilter("dateFormat", function(date, format) {
+        return DateTime.fromISO(date).toFormat(format);
+    });
+
     eleventyConfig
         .addPassthroughCopy("src/assets/css")
         .addPassthroughCopy("src/assets/images")
         .addPassthroughCopy("src/assets/js")
+        .addPassthroughCopy("src/files/")
         .addPassthroughCopy("src/admin");
 
     return {
+        markdownTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
         passthroughFileCopy: true,
         dir: {
             input: "src",
