6c933148d3e517b64df94d5a57d873e10e7bbcef
chrisgebert <cj@chrisgebert.org>
2024-09-18 22:30:20 -0400

    update

---
+++ src/_data/ languages.js
@@ -0,0 +1,9 @@
+module.exports = {
+    en: {
+        dir: '',
+        availableText: 'This pages is also available in:'
+    },
+    es: {
+        availableText: 'Esta página también está disponible en:'
+    }
+};
---
+++ src/_data/site.js
@@ -0,0 +1,12 @@
+module.exports = {
+    buildTime: new Date(),
+    baseUrl: "https://gwchildshsa.org",
+    name: "GW Childs HSA",
+    en: {
+        metaTitle: "GW Childs HSA",
+        metaDescription: ""
+    },
+    es: {
+        metaTitle: ""
+    }
+}
