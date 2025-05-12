8308cad08ad31dc96b200a28e41005b3c5f58b3f
chrisgebert <cj@chrisgebert.org>
2025-05-11 21:34:05 -0400

    remove dotenv

--- src/_data/hsaevents.js
+++ src/_data/hsaevents.js
@@ -1,14 +1,10 @@
 import Fetch from "@11ty/eleventy-fetch";
 
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const CALENDAR_ID = 'gwchildshsa@gmail.com';
 const API_KEY = process.env.GOOGLE_API_KEY;
 const TIME_MIN = new Date().toISOString();
 
-const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${GOOGLE_API_KEY}&timeMin=${TIME_MIN}&singleEvents=true&orderBy=startTime`;
+const url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}&timeMin=${TIME_MIN}&singleEvents=true&orderBy=startTime`;
 
 export default async function () {
   try {
